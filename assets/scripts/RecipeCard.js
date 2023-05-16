f76ce20d4cce6b8528b1954e4bcaad20ef8c86e0
AstroWang0306 <97696773+AstroWang0306@users.noreply.github.com>
2023-05-15 17:51:04 -0700

    Update RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,10 +8,25 @@ class RecipeCard extends HTMLElement {
 
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    const shadow = this.attachShadow({mode:'open'});
+    
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+    const article = document.createElement('article');
+    
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+    const style = document.createElement('style'); 
+    
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+    fetch('/assets/styles/main.css')
+      .then(response => response.text())
+      .then(css => {
+        style.textContent = css;
+      });
+    
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+    shadow.appendChild(style);
+    shadow.appendChild(article);
+    
   }
 
   /**
@@ -40,10 +55,13 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+    const article = this.shadowRoot.querySelector('article');
+    
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+    
   }
 }
 
